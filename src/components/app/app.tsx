c645caaaebd678add83d62fca2b7ca22d8fb3442
Yalanday <oeb72umvd@mial.ru>
2024-08-04 23:06:21 +0300

    complete task-2 react-router-dom

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Main from '../../pages/main/main';
-// import Main from '../../pages/main/main';
-// import Favorites from '../../pages/favorites/favorites';
-// import {Login} from '../../pages/login/login';
-// import Property from '../../pages/property/property';
+import Favorites from '../../pages/favorites/favorites';
+import {Login} from '../../pages/login/login';
+import Property from '../../pages/property/property';
+import NotFound from '../../pages/not-found/not-found';
+import PrivateRoute from '../../components/private-route/private-route';
+import {AppRoute, AuthorizationStatus} from '../../const';
 
 type AppProps = {
   offersCount: number;
 }
+
 function App({offersCount}: AppProps): JSX.Element {
-  return <Main offersCount={offersCount}/>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Main offersCount={offersCount}/>}/>
+        <Route path={AppRoute.Login} element={<Login/>}/>
+        <Route path={`${AppRoute.Property}/:id`} element={<Property/>}/>
+        <Route path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <Favorites />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
